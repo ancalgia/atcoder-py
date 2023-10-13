77bd44da19fc633839b1a355def2cf43b04a3cfc
ancalgia <105998552+ancalgia@users.noreply.github.com>
2023-10-13 17:26:25 +0900

    Add files via upload

--- src/abc160/b/main.py
+++ src/abc160/b/main.py
@@ -1,8 +1,18 @@
 #!/usr/bin/env python3
 
+import sys
+
+case: str = "".join([x for x in sys.stdin])
+
 
 def main():
-    pass
+    X = int(case)
+
+    gohyaku = X // 500
+
+    go = (X % 500) // 5
+
+    print(gohyaku * 1000 + go * 5)
 
 
 if __name__ == "__main__":
