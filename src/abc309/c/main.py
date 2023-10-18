28bf6e558d1d8e82072835fa9490bb7f69a0705b
ancalgia <105998552+ancalgia@users.noreply.github.com>
2023-10-18 17:57:17 +0900

    Add files via upload

--- src/abc309/c/main.py
+++ src/abc309/c/main.py
@@ -18,13 +18,34 @@ def main():
     (N, K), *ABs = IALL(case)
 
     left = 1
+    right = max([x[0] for x in ABs]) + 100
 
-    right = max([x[1] for x in ABs])
+    result = 9999999999999999999999
 
-    result = 9999999
+    sortedABs = sorted(ABs)
 
     while True:
+        if left == right:
+            print(result)
+            return
+
         tgt = (right + left) // 2
+
+        medicines = sum([x[1] for x in sortedABs if x[0] >= tgt])
+
+        if medicines <= K:
+            result = min([result, tgt])
+
+            if right == tgt:
+                right -= 1
+            else:
+                right = tgt
+
+        else:
+            if left == tgt:
+                left += 1
+            else:
+                left = tgt
 
     pass
 
