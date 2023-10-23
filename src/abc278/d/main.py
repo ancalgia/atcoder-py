f761edcc17f37b2a99a5c2a11283fddd9acc3950
ancalgia <ancalgia@gmail.com>
2023-10-24 01:49:38 +0900

    D2問

--- src/abc278/d/main.py
+++ src/abc278/d/main.py
@@ -18,15 +18,29 @@ case: str = "".join([x for x in sys.stdin])
 
 # case = dedent(
 #     """
-# 5
-# 3 1 4 1 5
-# 6
-# 3 2
-# 2 3 4
+# 10
+# 1 8 4 15 7 5 7 5 8 0
+# 20
+# 2 7 0
+# 3 7
+# 3 8
+# 1 7
 # 3 3
-# 1 1
-# 2 3 4
-# 3 3
+# 2 4 4
+# 2 4 9
+# 2 10 5
+# 1 10
+# 2 4 2
+# 1 10
+# 2 3 1
+# 2 8 11
+# 2 3 14
+# 2 1 9
+# 3 8
+# 3 8
+# 3 1
+# 2 6 5
+# 3 7
 #     """
 # ).strip()
 
@@ -38,28 +52,43 @@ def main():
 
     # dq.append([1] + As)
 
-    current = As
+    current = 0
 
-    addList = []
+    # addList = []
 
     addDict: dict[int, int] = dict()
+
+    for idx, x in enumerate(As, 1):
+        addDict[idx] = x
+
+    # currentValue = 0
 
     for x in Queries:
         if x[0] == 1:
             # dq.clear()
             # dq.append([1] + ([x[1]] * N))
-            current = [x[1]] * N
+            current = x[1]
             # addList.clear()
             addDict.clear()
         elif x[0] == 2:
-            addList.append([x[1], x[2]])
+            # addList.append([x[1], x[2]])
 
-            if x[1] in addDict:
-                addDict[x[1]] += x[2]
-            else:
-                addDict[x[1]] = x[2]
+            # if x[1] in addDict:
+            #     addDict[x[1]] += x[2]
+            # else:
+            #     addDict[x[1]] = x[2]
+
+            addDict.setdefault(x[1], 0)
+            addDict[x[1]] += x[2]
+
+            # if x[1] in addDict:
+            #     addDict[x[1]] += x[2]
+            # else:
+            #     addDict[x[1]] = x[2]
 
         else:
+            pass
+
             # tgt = list(dq.copy())
 
             # printTgtIndex = x[1]
@@ -73,7 +102,17 @@ def main():
             # counter = collections.Counter(addList)
             # addCount = sum([y[1] for y in addList if y[0] == x[1]])
 
-            print(current[x[1] - 1] + addDict.get(x[1], 0))
+            # current = [z + addDict.get(idx, 0) for idx, z in enumerate(current, 1)]
+
+            # # print(current[x[1] - 1] + addDict.get(x[1], 0))
+            # print(current[x[1] - 1])
+            # current[x[1] - 1] += addDict.get(x[1], 0)
+            # addDict[x[1]] = 0
+
+            # print(current[x[1] - 1] + addDict.get(x[1], 0))
+            print(current + addDict.get(x[1], 0))
+
+            # addDict.clear()
 
     pass
 
